8c53b647f7e584546c600c7eda5b98cf5a724a00
Stefano <stefano.lissiaeu@gmail.com>
2017-05-20 13:21:42 +0100

    first row completed js

--- README.md
+++ README.md
@@ -51,18 +51,10 @@ Adding an email capture form will make the call to action more prominent and wil
 //cdn.optimizely.com/img/153957092/3407bd50e311448e8e822aa345ccdbbc.png   
 
 ### alpha channel
-//cdn.optimizely.com/img/153957092/f19c0d82912849fd9c12a5a711cf5f35.png   
-//cdn.optimizely.com/img/153957092/d18ec7bccbb44ada994e36b40a0f316f.png
-//cdn.optimizely.com/img/153957092/b67883892bef450eb55a7a5d9a7d12f5.png
-//cdn.optimizely.com/img/153957092/7b496bc78a9745dd90960be6437e8906.png
-
-### alpha v2
-//cdn.optimizely.com/img/153957092/392492b278f74d9ea47973f696f2da4e.png
-
-//cdn.optimizely.com/img/153957092/8eca96af181b43b9b0dc1131cf4f8673.png
-
-//cdn.optimizely.com/img/153957092/8779b71d3a0e4262a5b4be2a0563e576.png
-
+//cdn.optimizely.com/img/153957092/7f27a97c11d5450c8c125396ba6332ac.png
+//cdn.optimizely.com/img/153957092/f90c13af55a948bf901b23d736d720ee.png
+//cdn.optimizely.com/img/153957092/92b91f66e86f431982bcd8366ffe6966.png
+//cdn.optimizely.com/img/153957092/6c64b72f53594f9897484470818f6f0a.png
 
 
 
--- js/one.js
+++ js/one.js
@@ -1,10 +1,11 @@
-(function(andRedEyelikeCityIndex, undefined) {
+(function(andRedEyelikeCityStockMkt, undefined) {
     var $ = window.jQuery;
     var SL = {};
-    SL.andRedEyelikeCityIndex = {
+    SL.andRedEyelikeCityStockMkt = {
         init: function() {
             this.pgCssDesktop();
             this.rowOne();
+            this.email();
             
           
            
@@ -13,7 +14,9 @@
         }, //init
         pgCssDesktop: function() {
             console.info('%c pgCssDesktop \u221a', 'background:blue;color:white;');
-            var mainCss = '.cola {width:50%; float:left;} .cola.otra li {margin-bottom:2em;} .cola.otra p {display:flex; max-width:350px; margin:0;} .cola a {text-decoration:none;} ul {list-style:none;} img.spread {float:left; margin-right:15px;} .hero-two-section.hero__headline {margin:0 !important;} ul.pad-top-m {padding:0 !important;} .hero-two-section[data-is-page-title=true] .hero__headline, .hero-two-section[data-is-page-title=True] .hero__headline {font-size:2.5em; font-weight:600;}';
+            var mainCss = '.enterEmail {float:left; display:inline-block; margin-right:15px; line-height:46px; vertical-align:middle; padding:15px 20px !important; height:46px !important; border-radius:3px !important; border:1px solid #e6e5e5 !important; width:55%; margin-top:.8em;} .cola {width:50%; float:left;} .cola.otra li {margin-bottom:2em;} .cta-btn.blue.cta-btn.green.APPEND_MARKETING_CID_COOKIE {margin:0 0 10px 0;} .ticon {float:left; }  .tlink {text-decoration:none; margin:0;} .tlink h4 {margin:0; display:inline-block; color:#000; font-weight:600;} .tlink p {margin:0; color:#202020; display:flex;} ul.bet {list-style:none;} ul.bet li {margin-bottom:3.5em;} .retrade {height:162px !important;} @media screen and (min-width:768px) { .beneton {display:none;} }  .bet {display:block;} .cola.otra p {display:flex; max-width:350px; margin:0;} .cola a {text-decoration:none;} ul {list-style:none;} img.spread {float:left; margin-right:15px;} .hero-two-section.hero__headline {margin:0 !important;} ul.pad-top-m {padding:0 !important;} .hero-two-section[data-is-page-title=true] .hero__headline, .hero-two-section[data-is-page-title=True] .hero__headline {font-size:2.5em; font-weight:600; margin-left:0;} .cola.otra h4 {line-height:10px; margin-bottom:5px; font-weight:bold;} .cola.otra p {line-height:18px;} .cola.otra h4:hover, .cola.otra p:hover, .cola.otra a, .cola.otra {color:#ffffff !important;}';
+
+
             var head = document.getElementsByTagName('head')[0];
 
             function addcss(css) {
@@ -34,7 +37,7 @@
             //div
             var div = document.createElement('div');
             div.className = "cola otra";
-            div.innerHTML = ' <ul> <li> <img class="spread" src="//cdn.optimizely.com/img/153957092/c84d833286fd4611b66e587611edc8a4.png" alt="Fixed 1 point spreads"> <a href="#" title="Fixed 1 point spreads"> <h4>Fixed 1 point spreads</h4> <p>On major indices in market hours including the UK 100 and Germany 30</p></a> </li><li> <img class="spread" src="//cdn.optimizely.com/img/153957092/254f7eeefefa487ea5f06e9a51c89027.png" alt="Tax-efficient trading"> <a href="#" title="Tax-efficient trading"> <h4>Tax-efficient trading</h4> <p>Pay no UK Capital Gains Tax on profits or stamp Duty when Spread Betting</p></a> </li><li> <img class="spread" src="//cdn.optimizely.com/img/153957092/e069819b59474dfb897d387efe1f6763.png" alt="Award Winning Platform"> <a href="#" title="Award Winning Platform"> <h4>Award Winning Platform</h4> <p>Voted Best Spread Betting Platform 2017 ADVFN</p></a> </li><li> <img class="spread" src="//cdn.optimizely.com/img/153957092/3407bd50e311448e8e822aa345ccdbbc.png" alt="Short the markets"> <a href="#" title="Short the markets"> <h4>Short the markets</h4> <p>Trade on falling markets (going short) as well as rising markets</p></a> </li></ul> ';
+            div.innerHTML = ' <ul> <li> <img class="spread" src="//cdn.optimizely.com/img/153957092/52b38d9275414c69afc689b528b09263.png" alt="Fixed 1 point spreads"> <a href="#" title="Fixed 1 point spreads"> <h4>Fixed 1 point spreads</h4> <p>On major indices in market hours including the UK 100 and Germany 30</p></a> </li><li> <img class="spread" src="//cdn.optimizely.com/img/153957092/254f7eeefefa487ea5f06e9a51c89027.png" alt="Tax-efficient trading"> <a href="#" title="Tax-efficient trading"> <h4>Tax-efficient trading</h4> <p>Pay no UK Capital Gains Tax on profits or stamp Duty when Spread Betting</p></a> </li><li> <img class="spread" src="//cdn.optimizely.com/img/153957092/e069819b59474dfb897d387efe1f6763.png" alt="Award Winning Platform"> <a href="#" title="Award Winning Platform"> <h4>Award Winning Platform</h4> <p>Voted Best Spread Betting Platform 2017 ADVFN</p></a> </li><li> <img class="spread" src="//cdn.optimizely.com/img/153957092/3407bd50e311448e8e822aa345ccdbbc.png" alt="Short the markets"> <a href="#" title="Short the markets"> <h4>Short the markets</h4> <p>Trade on falling markets (going short) as well as rising markets</p></a> </li></ul> ';
 
             xwrapper.appendChild(div);
 
@@ -48,8 +51,16 @@
 
         },//rowOne
 
-        rowTwo: function() {
-            console.info('%c rowTwo \u221a', 'background:blue;color:white;');
+        email: function() {
+            console.info('%c email \u221a', 'background:blue;color:white;');
+            var email = document.createElement('input');
+            email.setAttribute('type', 'text');
+            email.name = 'email';
+            email.className = "enterEmail";
+            email.setAttribute('placeholder', 'Enter your email address');
+            var trade = document.
+            querySelectorAll('.blue.APPEND_MARKETING_CID_COOKIE.cta-btn ')[0];
+            trade.parentElement.insertBefore(email,trade);
           
         },//rowTwo
 
@@ -79,7 +90,7 @@
     (function() {
 
         setTimeout(function() {
-            SL.andRedEyelikeCityIndex.init();
+            SL.andRedEyelikeCityStockMkt.init();
         }, 10);
 
     })();
@@ -87,4 +98,4 @@
 
 
 
-}.call(window.andRedEyelikeCityIndex || {}));
+}.call(window.andRedEyelikeCityStockMkt || {}));
